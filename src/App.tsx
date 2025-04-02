ee879df66fee66b19832a437a911f798f1451881
Parth Patel <parth.patel.reversebits@gmail.com>
2025-04-02 18:22:32 +0530

    ✨feature: Created Infinite Scroll of movies using Infinite Scroll and props drilling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
 import Card from "./components/card"
-
+import { useEffect, useState } from "react"
+import axios from "axios"
 const App = () => {
+
+  const [ data, setData ]  = useState([]);
+  const [page,setPage]=useState(1);
+
+  useEffect(()=>{
+    fetchData()
+  },[page])
+
+    const fetchData =async()=>{
+      const url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`;
+      const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYWY0ZmVlOTJhMjk4ZmIzOTZiYzA4YmRmMDNiNjIxNyIsIm5iZiI6MTc0MzUxMTIyOC42OTI5OTk4LCJzdWIiOiI2N2ViZGViYzM4NWVhMDFiODk3YWIxMTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.74LkItcRZleG4aoOsPv5GZj0AWTu5W17u2gwMyh_k5M';
+  
+      try {
+          const response = await axios.get(url, {
+          headers: {
+              Authorization: `Bearer ${token}`
+          }});
+          const apiData=response.data.results.map((item)=>{
+            // console.log(item.release_date)
+            return  {
+                "movieName": item.original_title,
+                "description": item.overview,
+                "imdbRatings": item.vote_average,
+                "imageUrl": `https://image.tmdb.org/t/p/original${item.poster_path}`,
+                "releaseDate": item.release_date,
+              }
+              
+        })
+        setData((prevData) => [...prevData, ...apiData]);
+      } catch (error) {
+          console.error('Error:', error);
+      }
+    }
+
   return(
     <>
-      <div className="w-full h-screen dark:bg-black dark:text-white">
-        Hello World
-        <Card/>
+      <div className="w-full  dark:bg-black dark:text-white">
+        <h1 className="text-center text-4xl pt-5 font-extrabold text-[#E2B616]">IMDB Infinite Movies Scrolling</h1>
+        <Card data={data} fetchData={fetchData} setPage={setPage} page={page}/>
       </div>
     </>
   )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,60 @@
-import getData, { finalArray }  from "../API-Services/FetchMovieData"
+import React from "react";
+import Star from "../../public/star.png";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-const card = () => {
-    return(
-        <>
-        {finalArray.map((item) => <h1>{item.movieName}</h1>)}
-        <h1>Hello I am card</h1>
-        </>
-    )
-}
+const Card = ({ data,fetchData,setPage,page } ) => {
+  const refresh = () => {
+    console.log("Refreshing data...");
+  };
 
-export default card
+  return (
+    <InfiniteScroll
+      dataLength={data.length} // Use data.length or items.length
+      next={() => {
+        setPage(page + 1); // Increment the page number
+        fetchData(); // Fetch more data for the next page
+      }}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+      refreshFunction={refresh}
+      pullDownToRefresh
+      pullDownToRefreshThreshold={50}
+      pullDownToRefreshContent={
+        <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
+      }
+      releaseToRefreshContent={
+        <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
+      }
+    >
+      <div>
+        {data.map((item, index) => (
+          <div key={index} className="flex mt-10 mx-50">
+            <img
+              className="w-[200px] rounded-2xl"
+              src={item.imageUrl}
+              alt={item.movieName}
+            />
+            <div className="w-full bg-[#161F30] p-5 rounded-2xl">
+              <h1 className="text-3xl font-bold">
+                {index + 1}. {item.movieName}
+              </h1>
+              <p className="mt-5">{item.description}</p>
+              <div className="flex items-center mt-5">
+                <img className="w-5" src={Star} alt="star" />
+                <div className="ml-3">IMDB Ratings: {item.imdbRatings}</div>
+              </div>
+              <div className="mt-5">Released Date: {item.releaseDate}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </InfiniteScroll>
+  );
+};
+
+export default Card;
